848fe22141a0b7d24c9204e717c2b1b5c4ee45ec
m.kurahara <m.kurahara@team.nri-net.com>
2020-01-29 15:57:39 +0900

    ・再インストール作業

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
-import {FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
