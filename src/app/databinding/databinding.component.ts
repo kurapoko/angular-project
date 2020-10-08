d73a36b13daa7e54660395ec08e2fbda6ae20fe5
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-08 23:56:47 +0900

    プロパティバインディング

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-databinding',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabindingComponent implements OnInit {
 
-  constructor() { }
+  name = 'Angular'
+
+  list = [
+    {id: 1, name: 'kurahara'},
+    {id: 2, name: 'masahiro'}
+  ]
+
+  image = 'http://www.wings.msn.to/image/wings.jpg';
+
+  safeMsg: SafeHtml;
+  safeUrl: SafeResourceUrl;
+
+  sanitizeHtml: string = `
+    <div>
+      <p>WINGS プロジェクト</p>
+    </div>
+    <a href="https://www.yahoo.co.jp/">Yahoo.co.jp</a>
+    <button>同意する</button>
+    <input type="button" onclick="alert('OK')" value="クリック">
+  `
+
+  sanitizeUrl = 'https://www.yahoo.co.jp/';
+
+  constructor (private sanitizer: DomSanitizer ) {
+    this.safeMsg = sanitizer.bypassSecurityTrustHtml(this.sanitizeHtml);
+    this.safeUrl = sanitizer.bypassSecurityTrustResourceUrl(this.sanitizeUrl);
+  }
 
   ngOnInit() {
   }
