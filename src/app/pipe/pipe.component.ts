511a782d401d047df8ca6f4ad18074870d508001
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-10 20:47:24 +0900

    パイプとか

--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipeComponent implements OnInit {
 
+  title = 'K U R A H A R A masahiro';
+
+  object: any = {
+    name: 'kurahara',
+    gender: undefined,
+    birth: new Date(1990, 6,22),
+    age: 30,
+    family: ['父', '母', '兄'],
+    work: function(){/* メソッドの中身*/},
+    other:
+    {
+      favorite: 'ひまわり',
+      memo: '強い',
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
