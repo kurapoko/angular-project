6e9972c8a585b91dae01c2b26556fa554169dbae
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-10 21:43:31 +0900

    form基礎

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  user = {
+    mail: 'hogehoge@mail.com',
+    passwd: '',
+    name: '山田太郎',
+    memo: 'メモ'
+  };
+
   constructor() { }
 
   ngOnInit() {
+    console.log('')
   }
 
 }
