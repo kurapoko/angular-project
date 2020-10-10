02e0ba6b5df99fef4731aaa4865514ea6d9c9bc7
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-10 22:18:12 +0900

    モデル駆動型フォーム

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,17 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  user = {
-    mail: 'hogehoge@mail.com',
-    passwd: '',
-    name: '山田太郎',
-    memo: 'メモ'
-  };
+  mail = new FormControl('hogehoge@mail.com', [
+    Validators.required,
+    Validators.email
+  ]);
+  passwd = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  name = new FormControl('山田太郎', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(10)
+  ]);
+  memo = new FormControl('メモ', [
+    Validators.maxLength(18)
+  ]);
 
-  constructor() { }
+  // FormGroupオブジェクト
+
+  myForm = this.builder.group({
+    maill: this.mail,
+    passwd: this.passwd,
+    name: this.memo,
+    memo: this.memo
+  });
+
+  constructor(private builder: FormBuilder ) { }
 
   ngOnInit() {
     console.log('')
   }
+
+  show() {
+    console.log('メールアドレス: ' + this.mail.value);
+    console.log('パスワード: ' + this.passwd.value);
+    console.log('名前：（漢字）:' + this.name.value);
+    console.log('備考：' + this.memo.value);
+    console.log('全て:');
+    console.log(this.myForm.value);
+  }
+
 
 }
